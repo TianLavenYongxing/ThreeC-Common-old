3ac3d55def4ca728cbb6f5529d1231e1f1ec3149
laven <tianlavenyongxing@gmail.com>
2025-05-29 22:30:53 +0800

    feat(common-mybatis): 为 BaseEntity 类的 id 字段指定 ID生成策略
    - 在 BaseEntity 类中，将 @TableId 注解的 id 字段修改为 @TableId(type = IdType.ASSIGN_ID)
    - 这一修改指定了使用 MyBatis-Plus 的 ASSIGN_ID 策略生成 ID，确保了 ID 的唯一性和高效性

--- threec-common-mybatis/src/main/java/com/goarchery/common/mybatis/entity/BaseEntity.java
+++ threec-common-mybatis/src/main/java/com/goarchery/common/mybatis/entity/BaseEntity.java
@@ -19,7 +19,7 @@ public class BaseEntity implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    @TableId
+    @TableId(type = IdType.ASSIGN_ID)
     private Long id;
     /**
      * 创建人
