7aac08e357ef0cc3414494e76f309e32c32e96ee
laven <tianlavenyongxing@gmail.com>
2025-09-21 15:54:47 +0800

    feat(threec-common-redis): 移除默认缓存配置移除了 Redis 缓存管理器中默认的缓存配置，包括 CompCategoryContent:isAllowMod和 user 的缓存设置，以便在其他地方进行更灵活的配置。

--- threec-common-redis/src/main/java/com/goarchery/common/config/RedisCacheConfiguration.java
+++ threec-common-redis/src/main/java/com/goarchery/common/config/RedisCacheConfiguration.java
@@ -19,10 +19,6 @@ public class RedisCacheConfiguration {
     public CacheManager cacheManager(RedissonClient redissonClient) {
         // 定义不同 cacheName 的 TTL 配置
         Map<String, CacheConfig> config = new HashMap<>();
-        // 默认缓存 12 小时，最大空闲时间 30 分钟
-        config.put("CompCategoryContent:isAllowMod", new CacheConfig(12 * 60 * 60 * 1000L, 30 * 60 * 1000L));
-        config.put("user", new CacheConfig(6 * 60 * 60 * 1000L, 10 * 60 * 1000L));
-
         return new RedissonSpringCacheManager(redissonClient, config);
     }
 }
